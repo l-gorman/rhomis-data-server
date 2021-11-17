6c9f8552ca50c42dbb662524944c9ed6f21f3ad1
l-gorman <lgorman@turing.ac.uk>
2021-11-17 09:41:04 +0000

    removing permissions

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@
 # mkdir -p ./files/certbot/www
 # mkdir -p ./files/certbot/conf
 mkdir -p data/nginx
-
+sudo chmod -rwx data/nginx
 
 # Load environment variables
 source .env
@@ -18,19 +18,16 @@ source .env
 #     done < ./files/nginxTemplate/conftemplate.d > ./files/nginx/conf.d
 IFS=''
 
-touch ./data/nginx/app.conf
 while read line ; do 
     echo "${line//example.org/$DATAURL}"; 
     done < ./templates/conf.d.template > ./data/nginx/app.conf
 
-touch
 while read -r line; do 
     line="${line//user@email.com/$ADMINEMAIL}"
 
     echo "${line//example.org/$DATAURL}"; 
     done < ./templates/docker-compose-template.yml > ./docker-compose.yml
 
-touch ./init-letsencrypt.sh 
 while read line ; do 
     line="${line//user@email.com/$ADMINEMAIL}"
     echo "${line//example.org/$DATAURL}"; 
